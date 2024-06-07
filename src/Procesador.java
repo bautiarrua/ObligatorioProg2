6ce794dc8ad7c14f3610078f352442282206acae
bautiarrua <bautista.arruabarrena@gmail.com>
2024-06-07 19:54:02 -0300

    prueba

--- src/Procesador.java
+++ src/Procesador.java
@@ -23,20 +23,17 @@ public class Procesador {
         MyArbolbinario<ChronoLocalDate, MyHashCerradoI<String, Top50>> arbolFechas = new Arbolbinario<>();
         Cancion c;
         Top50 toptemp = new Top50(null, null);
-        int size =53;
+        int size =750000;
         int ca = 2;
         int l =0;
         //String fileName = "C:\\Users\\jarruabarrena\\OneDrive - Universidad de Montevideo\\Prog2\\Obligatorio\\universal_top_spotify_songs.csv";
-        String fileName = "C:\\Users\\franc\\OneDrive - Universidad de Montevideo\\Programación 1\\universal_top_spotify_songs";
+        String fileName = "C:\\Users\\jarruabarrena\\OneDrive - Universidad de Montevideo\\Prog2\\Obligatorio\\universal_top_spotify_songs.csv";
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String linea = br.readLine();
 //            while ((linea = br.readLine()) != null){
             for (int i = 0; i < size; i++) { // Este for es para decirle cuantas lineas leer
-                System.out.println(ca);
                 linea = br.readLine();
                 if (linea != null) {
-                    ca++;
-
                     //Obtener los datos
                     String[] datos = linea.split(",\"");
 
@@ -51,7 +48,7 @@ public class Procesador {
 
                     c = new Cancion(datos[0].replaceAll("\"", ""),
                             datos[1].replaceAll("\"", ""),
-                            Float.parseFloat(datos[24].replaceAll("\"", "")));
+                            Float.parseFloat(datos[23].replaceAll("\"", "")));
 
                     for (int a = 0; a < artistas.length; a++) { // Este for es para agregar los artistas a la cancion
                         c.getArtist().add(artistas[a].replaceAll("[\"; ]", ""));
@@ -87,7 +84,6 @@ public class Procesador {
                                     arbolFechas.find(fecha).get(pa).setTop(toptemp.getTop());
                                     l++;
                                 } catch (Exception e) {
-                                    System.out.println("no agrego el top lleno");
                                 }
                             }
 
@@ -110,7 +106,7 @@ public class Procesador {
             }
             System.out.println("Los paises rgitrados en esta fecha son: ");
             for(int p = 0; p<arbolFechas.find(fecha).Keys().size(); p++){
-                System.out.println(arbolFechas.find(fecha).Keys().get(p));
+                System.out.print(arbolFechas.find(fecha).Keys().get(p) + " || ");
             }
             String pai = arbolFechas.find(fecha).Keys().get(1);
             System.out.println(arbolFechas.find(fecha).get(pai).getTop().delete().getName() );
